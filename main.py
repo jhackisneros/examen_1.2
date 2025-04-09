94e6a07b666fcdb8cbdaab58f5c2373c1e300b98
WhiteSpaghet <superivasca@gmail.com>
2025-04-09 10:35:12 +0200

    main2juegos

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from juego_De_caballo.tablero import TableroVisual
+from juego_reina.algoritmo import resolver_n_reinas
+from juego_reina.guardarsoluciones import guardar_soluciones
 import random
 
 def juego_de_caballo():
@@ -10,5 +12,30 @@ def juego_de_caballo():
     tablero_visual = TableroVisual()
     tablero_visual.ejecutar(inicio_x, inicio_y)
 
+def juego_de_reinas():
+    print("¡Bienvenido al problema de las n-reinas!")
+    N = int(input("Introduce el tamaño del tablero (N): "))
+    soluciones = resolver_n_reinas(N)
+    print(f"Se encontraron {len(soluciones)} soluciones")
+
+    # Guardar las soluciones en el archivo CSV
+    archivo_csv = "soluciones_reinas.csv"
+    guardar_soluciones(N, soluciones, archivo_csv)
+
 if __name__ == "__main__":
-    juego_de_caballo()
+    while True:
+        print("\nSeleccione el juego que desea jugar:")
+        print("1. Problema del caballo")
+        print("2. Problema de las n-reinas")
+        print("3. Salir")
+        opcion = input("Ingrese el número de su elección: ")
+
+        if opcion == "1":
+            juego_de_caballo()
+        elif opcion == "2":
+            juego_de_reinas()
+        elif opcion == "3":
+            print("¡Gracias por jugar!")
+            break
+        else:
+            print("Opción no válida. Por favor, intente de nuevo.")
